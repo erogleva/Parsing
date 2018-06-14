379a3524acc9f54ea8ed634b229c941d1d384166
erogleva <e.rogleva@mail.bg>
2018-06-14 09:29:58 +0200

    fix

--- cyk_chart.py
+++ cyk_chart.py
@@ -1,6 +1,5 @@
 from classes import Backpointer, BinaryTree
 
-trees = []
 
 def _find_substring_matches(table_cell, table, rules):
     # helper function for the construction of the CYK-chart:
@@ -61,13 +60,12 @@ def print_parse_tree(tree, indent):
         print_parse_tree(tree.right, indent)
 
 
-
 def _create_tree(data, cell, chart, string):
     tree = BinaryTree()
     tree.data = data
 
     if cell[0] != 0:
-        bp = chart[cell[0]][cell[1]][data][0]
+        bp = chart[cell[0]][cell[1]][data][0] # TODO
         tree.left = _create_tree(bp.rule.RHS[0], bp.cell1, chart, string)
         tree.right = _create_tree(bp.rule.RHS[1], bp.cell2, chart, string)
     else:
@@ -78,11 +76,6 @@ def _create_tree(data, cell, chart, string):
 
 def parse(chart, string):
     parse_tree = _create_tree('S', [len(chart) - 1, 0], chart, string)
-
-    global trees
-    for tree in trees:
-        print_parse_tree(tree, '')
-        print('---------------------------------------------------------------------------------------')
     print_parse_tree(parse_tree, 0)
     return parse_tree
 
